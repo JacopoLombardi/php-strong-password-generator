39a5846e0719654c66e64efba877b51d4d05a1dc
JacopoLombardi <lombajacopo@gmail.com>
2024-04-16 16:42:48 +0200

    [add] Milestone 3

--- index.php
+++ index.php
@@ -2,14 +2,18 @@
 <!-- PHP -->
 <?php
 
-   // import file php
-   require __DIR__ . '/data/array.php';
-   require __DIR__ . '/data/functions.php';
+   if(isset($_GET['length'])){
 
-   $length_pwd = $_GET['length'];
+      if($_GET['length'] < 6){
+         $error_msg = '';
+         $error_msg = 'Inserire una Password più lunga!!';
 
-   // function
-   $pwd = pwd_generator($length_pwd, $characters, $pwd);
+      }else{
+         session_start();
+         $_SESSION['length'] = $_GET['length'];
+         header('Location: ./landing_page.php');
+      };
+   };
 
 ?>
 
@@ -18,7 +22,7 @@
 <!-- HTML -->
 <?php require __DIR__ . '/partials/head.php' ?>
 
-<body>
+<body class="bg-light">
 
    <div class="container mt-5">
       <h1 class="mb-4">Strong Password Generator</h1>
@@ -33,8 +37,9 @@
          </form>
       </div>
 
-      <!-- pwd -->
-      <h3> <?php echo $pwd ?></h3>
+      <!-- error -->
+      <h3><?php echo $pwd ?></h3>
+      <h3 class="text-danger"> <?php echo $error_msg ?></h3>
 
    </div>
 
---
+++ landing_page.php
@@ -0,0 +1,35 @@
+
+<!-- PHP -->
+<?php
+
+   // import file php
+   require __DIR__ . '/data/array.php';
+   require __DIR__ . '/data/functions.php';
+ 
+   session_start();
+
+   if(isset($_SESSION['length'])){
+      $length_pwd = $_SESSION['length'];
+
+      // function
+      $pwd = pwd_generator($length_pwd, $characters, $pwd);
+   };
+
+?>
+
+
+
+<!-- HTML -->
+<?php require __DIR__ . '/partials/head.php' ?>
+
+<body class="bg-light">
+
+   <div class="container mt-5">
+
+      <!-- pwd -->
+      <h3>La tua Password é: <strong> <?php echo $pwd ?> </strong></h3>
+
+   </div>
+
+</body>
+</html>
